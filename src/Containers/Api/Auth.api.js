0bcb265c8ea0282b4c747f8d16abcef88df31e2c
ayushsutariya <ayushsutariya09gmail.com>
2022-08-23 15:49:57 +0530

    email_verified

--- src/Containers/Api/Auth.api.js
+++ src/Containers/Api/Auth.api.js
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, sendEmailVerification } from "firebase/auth";
 import { auth } from "../../Firebase/Firebase";
 
 export const SignupApi = (data) => {
@@ -8,15 +8,33 @@ export const SignupApi = (data) => {
             // Signed in 
             const user = userCredential.user;
 
-            console.log(user);
-            // ...
+            onAuthStateChanged(auth, (user) => {
+                if (user) {
+                    sendEmailVerification(user)
+                }
+              });
+        })
+        .then((user) => {
+            // Signed in 
+            // const user = userCredential.user;
+
+            onAuthStateChanged(auth, (user) => {
+                if (user.emailVerified) {
+                    console.log("Email is Successfully verified");
+                } else{
+                    console.log("Check your Email");
+                }
+              });
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
 
-            console.log(errorCode, errorMessage);
-            // ..
-        });
+            if(errorCode.localeCompare("auth/email-already-in-use") === 0){
+                console.log("Email is Already Registered");
+            } else {
+                console.log(errorCode);
+            }
+});
 
 }
